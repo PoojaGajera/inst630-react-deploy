5b8b0c9c2611c7eb9bb339501396a0b5cbb16a24
Pooja <poojag23.pg@gmail.com>
2022-10-05 09:50:37 -0400

    Updates

---
+++ precache-manifest.6f9955eb50dfe5206414359b29041990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8032c8ddcc2bcd5b666e",
+    "url": "/inst630-react-deploy/static/css/main.16a8176a.chunk.css"
+  },
+  {
+    "revision": "8032c8ddcc2bcd5b666e",
+    "url": "/inst630-react-deploy/static/js/main.1b148a83.chunk.js"
+  },
+  {
+    "revision": "39bbef03ce1c21cf0dd2",
+    "url": "/inst630-react-deploy/static/js/runtime~main.f0f4a1c8.js"
+  },
+  {
+    "revision": "d9341d9d62a007e46cf9",
+    "url": "/inst630-react-deploy/static/js/2.d4b86167.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/inst630-react-deploy/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "fe692426f75a69287bcad681765a928b",
+    "url": "/inst630-react-deploy/index.html"
+  }
+];
